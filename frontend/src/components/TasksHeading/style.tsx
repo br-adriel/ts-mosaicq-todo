1a4830dcb4d648be2f7f8e208f07b4b48afcac14
Adriel <adriel.fsantos@outlook.com>
2024-05-28 14:43:05 -0300

    feat: Add TasksHeading component

---
+++ frontend/src/components/TasksHeading/style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  display: flex;
+  gap: 8px;
+
+  h2 {
+    flex-grow: 1;
+    padding: 16px;
+    border-radius: 8px;
+    border: 1px solid var(--border-color);
+  }
+`;
