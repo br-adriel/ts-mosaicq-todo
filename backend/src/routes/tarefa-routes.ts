f06b2b570276e4b782de1679bea9fab247093429
Adriel <adriel.fsantos@outlook.com>
2024-05-28 01:00:24 -0300

    feat: Add route for creating Tarefa object

--- backend/src/routes/tarefa-routes.ts
+++ backend/src/routes/tarefa-routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import TarefaController from '../controllers/tarefa-controller';
+import { validateData } from '../middlewares/validation-middleware';
+import { createTarefaSchema } from '../schemas/tarefa-schemas';
 
 const tarefaRoutes = Router();
 
 tarefaRoutes.get('/', TarefaController.getAll);
 tarefaRoutes.get('/:id', TarefaController.getOne);
+tarefaRoutes.post(
+  '/',
+  validateData(createTarefaSchema),
+  TarefaController.create
+);
 
 export default tarefaRoutes;
