ad91fba33192d7c3e8fa0e0687795727bb4d3892
Adriel <adriel.fsantos@outlook.com>
2024-05-28 13:19:22 -0300

    feat: Setup cors and helmet for backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,20 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 import tarefaRoutes from './routes/tarefa-routes';
 
 const app = express();
 
+// ajustes de segurança
+app.use(helmet());
+app.disable('x-powered-by');
+
 // middlewares
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
