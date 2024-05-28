8580d78063c45d983b3652d7d747f7f3297b1cdf
Adriel <adriel.fsantos@outlook.com>
2024-05-28 13:31:01 -0300

    feat: Setup react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
-  async function fetchTasks() {
-    const res = await fetch('http://localhost:3000/tarefas');
-    const tasks = await res.json();
-    console.log(tasks);
-  }
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-  return <h1>Olá mundo</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
