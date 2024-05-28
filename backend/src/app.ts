8d2f8fc0fd786cbdbb6710c75572c051923d8e89
Adriel <adriel.fsantos@outlook.com>
2024-05-28 00:59:25 -0300

    feat: Setup middlewares for loading json data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,11 @@ import tarefaRoutes from './routes/tarefa-routes';
 
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// rotas
 app.use('/tarefas', tarefaRoutes);
 
 export default app;
