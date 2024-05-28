568c02f9918f0f9048a6aff734e8955091f08ff9
Adriel <adriel.fsantos@outlook.com>
2024-05-28 00:21:45 -0300

    feat: Add routing for getting a Tarefa object

--- backend/src/routes/tarefa-routes.ts
+++ backend/src/routes/tarefa-routes.ts
@@ -4,5 +4,6 @@ import TarefaController from '../controllers/tarefa-controller';
 const tarefaRoutes = Router();
 
 tarefaRoutes.get('/', TarefaController.getAll);
+tarefaRoutes.get('/:id', TarefaController.getOne);
 
 export default tarefaRoutes;
