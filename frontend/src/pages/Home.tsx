d7ec0f89c3d9a8d803ce8de5c75e978ba6c9b576
Adriel <adriel.fsantos@outlook.com>
2024-05-28 19:36:20 -0300

    feat: Update Home page with its content

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,29 @@
+import styled from 'styled-components';
+import Container from '../components/Container';
+import TasksHeading from '../components/TasksHeading';
+import TasksList from '../components/TasksList';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Main>
+      <Container>
+        <TasksHeading />
+        <TasksList />
+      </Container>
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  margin: 24px 0;
+  padding: 0 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+
+  ${Container} {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+`;
