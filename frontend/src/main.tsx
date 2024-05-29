face0983f4c777f22b41f7fd7794cccd34b8fb39
Adriel <adriel.fsantos@outlook.com>
2024-05-28 23:26:43 -0300

    feat: Setup toastify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
 import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer position='bottom-center' closeOnClick />
   </React.StrictMode>
 );
