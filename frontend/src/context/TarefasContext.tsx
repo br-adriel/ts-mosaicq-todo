b2779904b767e5f703fd2af4171faa41675d1ad2
Adriel <adriel.fsantos@outlook.com>
2024-05-29 17:21:03 -0300

    feat: Add create and improve update methods on TarefasContext

--- frontend/src/context/TarefasContext.tsx
+++ frontend/src/context/TarefasContext.tsx
@@ -1,13 +1,15 @@
 import { ReactNode, createContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../lib/axios';
-import Tarefa from '../model/Tarefa';
+import Tarefa, { TarefaData } from '../model/Tarefa';
+import { sortByStatusAndDataCriacao } from '../utils/tarefas';
 
 interface TarefasContextData {
   tarefas: Tarefa[];
   isLoading: boolean;
   fetchAll: () => Promise<void>;
-  update: (tarefa: Tarefa) => Promise<void>;
+  create: (tarefa: TarefaData) => Promise<void>;
+  update: (id: string, tarefa: Partial<TarefaData>) => Promise<void>;
 }
 
 const TarefasContext = createContext<TarefasContextData>(
@@ -17,6 +19,14 @@ const TarefasContext = createContext<TarefasContextData>(
 interface IProps {
   children: ReactNode;
 }
+
+const handleError = (err: any) => {
+  if (err.response) {
+    toast.error('Um erro ocorreu: ' + err.response.data.error);
+  } else {
+    toast.error('Um erro ocorreu!');
+  }
+};
 
 export const TarefasProvider = ({ children }: IProps) => {
   const [tarefas, setTarefas] = useState<Tarefa[]>([]);
@@ -28,34 +38,47 @@ export const TarefasProvider = ({ children }: IProps) => {
       const { data } = await api.get('tarefas');
       setTarefas(data.tarefas);
     } catch (err: any) {
-      toast.error('Não foi possível recuperar as tarefas.');
+      handleError(err);
     } finally {
       setIsLoading(false);
     }
   };
 
-  const update = async (tarefa: Tarefa) => {
+  const create = async (tarefa: Omit<Omit<Tarefa, 'id'>, 'dataCriacao'>) => {
     try {
-      const { data } = await api.patch('tarefas/' + tarefa.id, tarefa);
+      setIsLoading(true);
+
+      const { data } = await api.post<Tarefa>('tarefas', tarefa);
+      setTarefas((prev) => {
+        const newTarefas = [...prev, data];
+        return newTarefas.sort(sortByStatusAndDataCriacao);
+      });
+
+      toast.success('Tarefa criada!');
+    } catch (err: any) {
+      handleError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const update = async (id: String, tarefa: Partial<TarefaData>) => {
+    try {
+      const { data } = await api.patch('tarefas/' + id, tarefa);
+      setIsLoading(true);
       setTarefas((prev) => {
         return prev
           .map((t) => {
             if (t.id == data.id) return data;
             return t;
           })
-          .sort((a, b) => {
-            if (a.status === b.status) {
-              return (
-                new Date(b.dataCriacao).getTime() -
-                new Date(a.dataCriacao).getTime()
-              );
-            } else {
-              return b.status.localeCompare(a.status);
-            }
-          });
+          .sort(sortByStatusAndDataCriacao);
       });
+      toast.success('Tarefa atualizada!');
     } catch (err: any) {
-      toast.error('Não foi possível atualizar a tarefa.');
+      handleError(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -65,6 +88,7 @@ export const TarefasProvider = ({ children }: IProps) => {
         tarefas,
         isLoading,
         fetchAll,
+        create,
         update,
       }}
     >
