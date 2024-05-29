70b19db7ec6e8a82517e38be417f04a0827b8852
Adriel <adriel.fsantos@outlook.com>
2024-05-29 17:19:09 -0300

    feat: Add TarefaData interface

--- frontend/src/model/Tarefa.ts
+++ frontend/src/model/Tarefa.ts
@@ -1,9 +1,11 @@
 export default interface Tarefa {
   id: string;
   titulo: string;
-  descicao: string;
+  descricao: string;
   status: TarefaStatus;
   dataCriacao: string;
 }
+
+export type TarefaData = Omit<Omit<Tarefa, 'id'>, 'dataCriacao'>;
 
 export type TarefaStatus = 'EM_PROGRESSO' | 'PENDENTE' | 'CONCLUIDA';
