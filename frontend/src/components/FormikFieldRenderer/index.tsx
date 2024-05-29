943b8c742a16d7a6159128837d2c20a1bfb7f95a
Adriel <adriel.fsantos@outlook.com>
2024-05-28 22:53:58 -0300

    feat: Add FormikFieldRenderer helper component

---
+++ frontend/src/components/FormikFieldRenderer/index.tsx
@@ -0,0 +1,30 @@
+import { ErrorMessage, Field, useField } from 'formik';
+import { FormField } from '../../interfaces/FormField';
+import Select from '../Select';
+import * as S from './style';
+
+interface IProps {
+  field: FormField;
+}
+
+export default function FormikFieldRenderer({ field }: IProps) {
+  const [_, meta, __] = useField(field.name);
+
+  return (
+    <S.FieldWrapper>
+      <label htmlFor={field.id}>{field.label}</label>
+
+      {field.type == 'select' ? (
+        <Select field={field} />
+      ) : (
+        <Field {...field} className={meta.error && meta.touched && 'error'} />
+      )}
+
+      {meta.touched && meta.error && (
+        <S.FormikError>
+          <ErrorMessage name={field.name} />
+        </S.FormikError>
+      )}
+    </S.FieldWrapper>
+  );
+}
---
+++ frontend/src/components/FormikFieldRenderer/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FieldWrapper = styled.div`
+  margin-bottom: 16px;
+  max-width: 100%;
+`;
+
+export const FormikError = styled.span`
+  color: red;
+  font-size: 0.8rem;
+`;
