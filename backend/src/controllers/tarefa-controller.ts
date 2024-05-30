7566a1cd04f0c8fa6a11bd1660f2ac11676bb052
Adriel <adriel.fsantos@outlook.com>
2024-05-30 00:37:10 -0300

    docs: Document api endpoints with swagger

--- backend/src/controllers/tarefa-controller.ts
+++ backend/src/controllers/tarefa-controller.ts
@@ -10,6 +10,14 @@ import {
 
 export default class TarefaController {
   static async getAll(req: Request, res: Response) {
+    /**
+    #swagger.tags = ['Tarefas']
+    #swagger.summary = 'Busca todas as tarefas'
+    #swagger.responses[200] = {
+      description: 'Retorna todas as tarefas.',
+      schema: { $ref: '#/components/schemas/tarefaAll' }
+    }
+    */
     const tarefas = await prisma.tarefa.findMany({
       orderBy: [{ status: 'asc' }, { dataCriacao: 'desc' }],
     });
@@ -17,6 +25,17 @@ export default class TarefaController {
   }
 
   static async getOne(req: Request, res: Response) {
+    /**
+    #swagger.tags = ['Tarefas']
+    #swagger.summary = 'Busca a tarefa com o id referente.'
+    #swagger.responses[200] = {
+      description: 'Retorna a tarefa solicitada.',
+      schema: { $ref: '#/components/schemas/tarefa' }
+    }
+    #swagger.responses[404] = {
+      description: 'Tarefa não encontrada.'
+    }
+    */
     const params = getOneTarefaParams.parse(req.params);
     const tarefa = await prisma.tarefa.findUnique({
       where: {
@@ -29,6 +48,24 @@ export default class TarefaController {
   }
 
   static async create(req: Request, res: Response) {
+    /**
+    #swagger.tags = ['Tarefas']
+    #swagger.summary = 'Cria uma nova tarefa.'
+    #swagger.requestBody = {
+      required: true,
+      schema: {
+        $ref: "#/components/schemas/tarefaCreateBody"
+      }
+    }
+    #swagger.responses[201] = {
+      description: 'Objeto criado com sucesso. Retorna o objeto criado.',
+      schema: { $ref: '#/components/schemas/tarefa' }
+    }
+    #swagger.responses[400] = {
+      description: 'Há algo de errado com a requisição.',
+      schema: { $ref: '#/components/schemas/error400' }
+    }
+    */
     const data = createTarefaSchema.parse(req.body);
     const tarefa = await prisma.tarefa.create({
       data: {
@@ -40,6 +77,27 @@ export default class TarefaController {
   }
 
   static async update(req: Request, res: Response) {
+    /**
+    #swagger.tags = ['Tarefas']
+    #swagger.summary = 'Atualiza uma tarefa'
+    #swagger.requestBody = {
+      required: true,
+      schema: {
+        $ref: "#/components/schemas/tarefaUpdateBody"
+      }
+    }
+    #swagger.responses[200] = {
+      description: 'Atualização realizada com sucesso. Retorna o objeto atualizado.',
+      schema: { $ref: '#/components/schemas/tarefa' }
+    }
+    #swagger.responses[400] = {
+      description: 'Há algo de errado com a requisição.',
+      schema: { $ref: '#/components/schemas/error400' }
+    }
+    #swagger.responses[404] = {
+      description: 'Tarefa não encontrada.'
+    }
+    */
     const params = updateTarefaParams.parse(req.params);
     const tarefa = await prisma.tarefa.findUnique({
       where: {
@@ -59,6 +117,16 @@ export default class TarefaController {
   }
 
   static async delete(req: Request, res: Response) {
+    /**
+    #swagger.tags = ['Tarefas']
+    #swagger.summary = 'Exclui uma tarefa.'
+    #swagger.responses[200] = {
+      description: 'Tarefa deletada com sucesso.'
+    }
+    #swagger.responses[404] = {
+      description: 'Tarefa não encontrada.'
+    }
+    */
     const params = deleteTarefaParams.parse(req.params);
     const tarefa = await prisma.tarefa.findUnique({
       where: {
