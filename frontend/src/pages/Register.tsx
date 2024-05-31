3445767f67bad0c64907efc9b24a06fec78ad965
Adriel <adriel.fsantos@outlook.com>
2024-05-31 19:52:02 -0300

    feat: Implement registration

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,16 +1,34 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 import AuthPageLayout from '../components/AuthPageLayout';
+import Loading from '../components/Loading';
+import AuthContext from '../context/AuthContext';
 import { registerForm } from '../forms/register-form';
+import { RegisterFormValues } from '../types/auth';
 
 export default function Register() {
+  const { isLoading, register } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const submit = (values: RegisterFormValues) => {
+    try {
+      register(values).then(() => navigate('/'));
+    } catch (err) {}
+  };
+
   return (
     <AuthPageLayout>
       <h2>Criar conta</h2>
-      <AuthForm
-        onSubmit={(values) => console.log(values)}
-        formObject={registerForm}
-        variant='cadastro'
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <AuthForm
+          onSubmit={submit}
+          formObject={registerForm}
+          variant='cadastro'
+        />
+      )}
     </AuthPageLayout>
   );
 }
